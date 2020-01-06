502505e601674682067236fced18f0ea7846a6dd
Gino Latorilla <pblatorilla+github@gmail.com>
2020-01-07 00:16:48 +0800

    Discover or accept paths and abort if they don't exist

--- ninja_log_analyser.py
+++ ninja_log_analyser.py
@@ -5,6 +5,7 @@ Copyright (c) 2020 ginolatorilla
 '''
 import argparse
 import sys
+import os
 
 
 def main():
@@ -14,6 +15,11 @@ def main():
 
 def get_program_options():
     parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-i',
+                        '--input-file',
+                        help='Path to a .ninja_log file.',
+                        default=os.path.join(os.getcwd(), '.ninja_log'),
+                        type=argparse.FileType('r'))
     return parser.parse_args()
 
 
--- tests/ninja_log_analyser_test.py
+++ tests/ninja_log_analyser_test.py
@@ -4,14 +4,57 @@ Copyright (c) 2020 Gino Latorilla.
 
 import pytest
 import subprocess
+import os
 
 from .testables import ninja_log_analyser
-
-
-def test_must_be_executable():
-    assert subprocess.run(ninja_log_analyser.__file__).returncode == 0
 
 
 def test_must_be_executable_with_builtin_help():
     result = subprocess.run([ninja_log_analyser.__file__, '-h'])
     assert result.returncode == 0
+
+
+def test_must_look_for_ninja_log_by_default_in_cwd(mocker):
+    mocker.patch('ninja_log_analyser.argparse.open')
+
+    mocker.patch('ninja_log_analyser.sys.argv', ['__main__'])
+    ninja_log_analyser.main()
+
+    ninja_log_analyser.argparse.open.assert_called_once_with(
+        os.path.join(os.getcwd(), '.ninja_log'), 'r', mocker.ANY, mocker.ANY,
+        mocker.ANY)
+
+
+def test_accept_ninja_log_path_as_input(mocker):
+    mocker.patch('ninja_log_analyser.argparse.open')
+
+    mocker.patch('ninja_log_analyser.sys.argv',
+                 ['__main__', '-i', '/path/to/.ninja_log'])
+    ninja_log_analyser.main()
+
+    ninja_log_analyser.argparse.open.assert_called_once_with(
+        '/path/to/.ninja_log', 'r', mocker.ANY, mocker.ANY, mocker.ANY)
+
+
+def test_abort_when_ninja_log_missing_in_cwd(mocker):
+    mocker.patch('ninja_log_analyser.argparse.open', side_effect=OSError)
+
+    with pytest.raises(SystemExit):
+        mocker.patch('ninja_log_analyser.sys.argv', ['__main__'])
+        ninja_log_analyser.main()
+
+    ninja_log_analyser.argparse.open.assert_called_once_with(
+        os.path.join(os.getcwd(), '.ninja_log'), 'r', mocker.ANY, mocker.ANY,
+        mocker.ANY)
+
+
+def test_abort_when_ninja_log_missing_in_input_args(mocker):
+    mocker.patch('ninja_log_analyser.argparse.open', side_effect=OSError)
+
+    with pytest.raises(SystemExit):
+        mocker.patch('ninja_log_analyser.sys.argv',
+                     ['__main__', '-i', '/bogus/path'])
+        ninja_log_analyser.main()
+
+    ninja_log_analyser.argparse.open.assert_called_once_with(
+        '/bogus/path', 'r', mocker.ANY, mocker.ANY, mocker.ANY)
