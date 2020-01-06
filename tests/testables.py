3a9065d3c01e1b5430470610f0f7f4aa2cda6082
Gino Latorilla <pblatorilla+github@gmail.com>
2020-01-06 22:38:40 +0800

    Seed files from python3-pipenv-template

---
+++ tests/testables.py
@@ -0,0 +1,11 @@
+'''
+Copyright (c) 2020 Gino Latorilla.
+'''
+
+import sys
+import os
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Add after this line the modules to be tested
+import ninja_log_analyser  # noqa: E402
